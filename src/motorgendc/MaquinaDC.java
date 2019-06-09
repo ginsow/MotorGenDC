95e71f730dfd037598e91c3818bf4cb1041a5b0c
ginsow <50158813+ginsow@users.noreply.github.com>
2019-06-08 20:54:00 -0400

    Add files via upload

--- src/motorgendc/MaquinaDC.java
+++ src/motorgendc/MaquinaDC.java
@@ -12,5 +12,139 @@ package motorgendc;
  */
 public class MaquinaDC {
     
-   
+    
+    //parametros
+    private double Vt;
+    private double IL;
+    private double H;
+    private double fi;
+    private double B;
+    private double A;
+    private double K;
+    private double L;
+    private double w;
+    private double Ieq;
+    
+    private double Rcarga;
+    
+    private double Va;
+    private double Ea;
+    private double Ra;
+    private double Ia;
+    private int Ns;
+    private double Rs;
+    private double Rarr;
+    
+    private double Rf;
+    private double Vf;
+    private int Nf;
+    private double If;
+    
+    private double Te;
+    private double Tf;
+    
+    private double Tabla[][]= {{0,1,2,3,4,5,6,7,8,9},{5,60,120,170,220,250,275,295,310,320}};
+    private double Wtabla;
+    private int Iftabla;
+    
+     
+    
+    public void generador(int tipo){
+        
+        
+        switch (tipo){
+            case 1:
+                //excitacion separada
+        
+                //in: Vf , W , Carga (o W y Vt) ; out: Vt (o Vf)
+                
+                
+                If= Vf/Rf;
+                Iftabla = (int)Math.round(If);
+                Ea= buscar(Tabla,Iftabla);
+                
+                
+                Ia= Ea / (Ra + Rcarga);
+                Vt = Ea - Ia*Ra;
+                
+                break;
+            case 2:
+                //en derivacion
+                
+                
+                /*
+                Ia = If+ IL;
+                Vt = Ea - Ia*Ra;
+                */
+                
+                break;
+            case 3: 
+                //serie
+                
+                /*
+                Vt = Ea - Ia*(Ra+Rs);
+                break;
+                */
+                break;
+            case 4:
+                 //compuesto
+                
+                /*
+                Ieq = If + (Ns/Nf)* Ia ;
+                */
+                
+                break;
+        }
+  
+    }
+    
+    public void motor(int tipo){
+        
+        
+        switch (tipo){
+            case 1:
+                //excitacion separada
+                
+                //in: Vt , Vf, Tf ; out W , TL
+                
+                If= Vf/Rf;
+                //Ea = K*fi*w;
+                Ea= buscar(Tabla, Iftabla);                
+                Ia= (Vt - Ea)/Ra ;
+                //Te = K*fi*Ia;
+                Te=Tf;
+                w= (Vt/(Ea/Wtabla))- Te*(Ra/Math.pow((Ea/Wtabla),2));
+
+                break;
+            case 2:
+                //en derivacion
+
+                break;
+                
+            case 3:                
+                //serie        
+                
+                break;                
+            case 4:
+                 //compuesto
+               
+                break; 
+    }   
+}
+    
+    public double buscar(double M[][], double I){
+    
+        for (int i = 0; i < M.length; i++) {
+            
+            if(I  == M[i][0]){
+                return M[1][i];
+            }
+            
+        }
+        
+        
+    return -1;
+    }
+    
+    
 }
